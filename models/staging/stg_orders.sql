9994b0d6192af2f57e6d3c66d250603e90c93813
Kishore K <kishore.kandula27@gmail.com>
2025-05-26 05:28:29 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,21 @@
+select 
+--raw_orders columns
+ro.orderid,
+ro.orderdate,
+ro.shipdate,
+ro.shipmode,
+ro.ORDERSELLINGPRICE - ro.ORDERCOSTPRICE ORDERPROFIT,
+ro.ORDERCOSTPRICE,
+ro.ORDERSELLINGPRICE,
+--raw_customer columns
+rc.customername,
+rc.segment,
+rc.country,
+--raw_product columns
+rp.category,
+rp.productname,
+rp.subcategory
+from {{ ref('raw_orders') }} ro,{{ ref('raw_customer') }} rc,{{ ref('raw_product') }} rp
+where 1 = 1
+and ro.customerid = rc.customerid (+)
+and ro.productid = rp.productid (+)
